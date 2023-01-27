bc15d1de15c1b66a8115de422dd926e034de7b20
wcc723 <agito723@gmail.com>
2023-01-27 11:12:22 +0800

    加入環境變數

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL, process.env.REACT_APP_API_PATH);
+    (async () => {
+      const res = await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`);
+      console.log(res);
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
