d46b19c8703847bdbbf72a0ee58eae6ce36ddd4f
wcc723 <agito723@gmail.com>
2023-02-24 19:01:11 +0800

    前台版型

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Dashboard from './pages/admin/Dashboard';
 import AdminProducts from './pages/admin/AdminProducts';
 import AdminCoupons from './pages/admin/AdminCoupons';
 import AdminOrders from './pages/admin/AdminOrders';
+import FrontLayout from './pages/front/FrontLayout';
+import Home from './pages/front/Home';
+import Products from './pages/front/Products';
 
 function App() {
 
   return (
     <div className='App'>
       <Routes>
+        <Route path='/' element={ <FrontLayout/>}>
+          <Route path='' element={<Home/>}></Route>
+          <Route path='products' element={<Products/>}></Route>
+        </Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/admin' element={<Dashboard />}>
           <Route path='products' element={<AdminProducts />}></Route>
