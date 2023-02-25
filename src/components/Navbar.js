1afe3084142b12019bf81c443809b33b85be2d85
wcc723 <agito723@gmail.com>
2023-02-25 15:13:49 +0800

    增加 Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className='bg-white sticky-top'>
+      <div className='container'>
+        <nav className='navbar px-0 navbar-expand-lg navbar-light bg-white'>
+          <NavLink
+            className='navbar-brand position-absolute'
+            to='/'
+            style={{
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              top: '50%',
+            }}
+          >
+            卡斯伯的日式餐點
+          </NavLink>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div
+            className='collapse navbar-collapse bg-white custom-header-md-open'
+            id='navbarNav'
+          >
+            <ul className='navbar-nav'>
+              <li className='nav-item active'>
+                <NavLink className='nav-link ps-0' to='/products'>
+                  產品列表
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='d-flex'>
+            <NavLink to='/cart' className='nav-link'>
+              <i className='bi bi-bag-fill'></i>
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './stylesheets/all.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
