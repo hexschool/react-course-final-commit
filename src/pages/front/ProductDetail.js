bb381f6ea4a1ae31d38fb8532064428ab942c155
wcc723 <agito723@gmail.com>
2023-02-26 10:35:13 +0800

    加入購物車

--- src/pages/front/ProductDetail.js
+++ src/pages/front/ProductDetail.js
@@ -4,7 +4,9 @@ import { useParams } from 'react-router-dom';
 
 function ProductDetail() {
   const [product, setProduct] = useState({});
+  const [cartQuantity, setCartQuantity] = useState(1);
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false)
 
   const getProduct = async (id) => {
     const productRes = await axios.get(
@@ -13,6 +15,27 @@ function ProductDetail() {
     console.log(productRes);
     setProduct(productRes.data.product);
   };
+
+  const addToCart = async () => {
+    const data = {
+      data: {
+        product_id: product.id,
+        qty: cartQuantity,
+      },
+    };
+    setIsLoading(true);
+    try {
+      const res = await axios.post(
+        `/v2/api/${process.env.REACT_APP_API_PATH}/cart`,
+        data,
+      );
+      console.log(res);
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
 
   useEffect(() => {
     getProduct(id);
@@ -24,8 +47,7 @@ function ProductDetail() {
       <div
         style={{
           minHeight: '400px',
-          backgroundImage:
-            `url(${product.imageUrl})`,
+          backgroundImage: `url(${product.imageUrl})`,
           backgroundPosition: 'center center',
         }}
       ></div>
@@ -33,15 +55,9 @@ function ProductDetail() {
         <div className='col-md-7'>
           <h2 className='mb-0'>{product.title}</h2>
           <p className='fw-bold'>NT$ {product.price}</p>
-          <p>
-            {product.content}
-          </p>
+          <p>{product.content}</p>
           <div className='my-4'>
-            <img
-              src={product.imageUrl}
-              alt=''
-              className='img-fluid mt-4'
-            />
+            <img src={product.imageUrl} alt='' className='img-fluid mt-4' />
           </div>
         </div>
         <div className='col-md-4'>
@@ -51,33 +67,41 @@ function ProductDetail() {
                 className='btn btn-outline-dark rounded-0 border-0 py-3'
                 type='button'
                 id='button-addon1'
+                onClick={() =>
+                  setCartQuantity((pre) => (pre === 1 ? pre : pre - 1))
+                }
               >
-                <i className='fas fa-minus'></i>
+                <i className='bi bi-dash'></i>
               </button>
             </div>
             <input
-              type='text'
+              type='number'
               className='form-control border-0 text-center my-auto shadow-none'
               placeholder=''
               aria-label='Example text with button addon'
               aria-describedby='button-addon1'
+              readOnly
+              value={cartQuantity}
             />
             <div className='input-group-append'>
               <button
                 className='btn btn-outline-dark rounded-0 border-0 py-3'
                 type='button'
                 id='button-addon2'
+                onClick={() => setCartQuantity((pre) => pre + 1)}
               >
-                <i className='fas fa-plus'></i>
+                <i className='bi bi-plus'></i>
               </button>
             </div>
           </div>
-          <a
-            href='./checkout.html'
-            className='btn btn-dark btn-block rounded-0 py-3'
+          <button
+            type='button'
+            className='btn btn-dark w-100 rounded-0 py-3'
+            onClick={() => addToCart()}
+            disabled={isLoading}
           >
-            Lorem ipsum
-          </a>
+            加入購物車
+          </button>
         </div>
       </div>
     </div>
