30b9532b2cce7d960674fe7347bf5949787eb7ae
wcc723 <agito723@gmail.com>
2023-02-27 11:27:22 +0800

    表單驗證、送出

--- src/pages/front/Checkout.js
+++ src/pages/front/Checkout.js
@@ -1,71 +1,107 @@
 import { Link, useOutletContext } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Input } from '../../components/FormElements';
+import axios from 'axios';
 
 function Checkout() {
   const { cartData } = useOutletContext();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onTouched',
+  });
+
+  const onSubmit = async (data) => {
+    const { name, email, tel, address } = data;    
+    const form = {
+      data: {
+        user: {
+          name,
+          email,
+          tel,
+          address,
+        },
+      },
+    };
+    const res = await axios.post(
+      `/v2/api/${process.env.REACT_APP_API_PATH}/order`,
+      form,
+    );
+    console.log(res);
+  };
 
   return (
     <div className='bg-light pt-5 pb-7'>
       <div className='container'>
         <div className='row justify-content-center flex-md-row flex-column-reverse'>
-          <form className='col-md-6'>
+          <form className='col-md-6' onSubmit={handleSubmit(onSubmit)}>
             <div className='bg-white p-4'>
               <h4 className='fw-bold'>外送資料</h4>
               <div className='mb-2'>
-                <label
-                  htmlFor='ContactMail'
-                  className='text-muted mb-0 form-label'
-                >
-                  Email
-                </label>
-                <input
+                <Input
+                  id='email'
+                  labelText='Email'
                   type='email'
-                  className='form-control rounded-0'
-                  id='ContactMail'
-                  aria-describedby='emailHelp'
-                  placeholder='example@gmail.com'
-                />
+                  errors={errors}
+                  register={register}
+                  rules={{
+                    required: 'Email 為必填',
+                    pattern: {
+                      value: /^\S+@\S+$/i,
+                      message: 'Email 格式不正確',
+                    },
+                  }}
+                ></Input>
               </div>
               <div className='mb-2'>
-                <label
-                  htmlFor='ContactName'
-                  className='text-muted mb-0 form-label'
-                >
-                  姓名
-                </label>
-                <input
+                <Input
+                  id='name'
                   type='text'
-                  className='form-control rounded-0'
-                  id='ContactName'
-                  placeholder='Carmen A. Rose'
-                />
+                  errors={errors}
+                  labelText='使用者名稱'
+                  register={register}
+                  rules={{
+                    required: '使用者名稱為必填',
+                    maxLength: {
+                      value: 10,
+                      message: '使用者名稱長度不超過 10',
+                    },
+                  }}
+                ></Input>
               </div>
               <div className='mb-2'>
-                <label
-                  htmlFor='ContactPhone'
-                  className='text-muted mb-0 form-label'
-                >
-                  聯絡電話
-                </label>
-                <input
-                  type='text'
-                  className='form-control rounded-0'
-                  id='ContactPhone'
-                  placeholder='0933-123-123'
-                />
+                <Input
+                  id='tel'
+                  labelText='電話'
+                  type='tel'
+                  errors={errors}
+                  register={register}
+                  rules={{
+                    required: '電話為必填',
+                    minLength: {
+                      value: 6,
+                      message: '電話不少於 6 碼',
+                    },
+                    maxLength: {
+                      value: 12,
+                      message: '電話不超過 12 碼',
+                    },
+                  }}
+                ></Input>
               </div>
               <div className='mb-2'>
-                <label
-                  htmlFor='inputCity'
-                  className='text-muted mb-0 form-label'
-                >
-                  外送地址
-                </label>
-                <input
-                  type='text'
-                  className='form-control rounded-0 mt-1'
-                  id='inputCity'
-                  placeholder='Address'
-                />
+                <Input
+                  id='address'
+                  labelText='地址'
+                  type='address'
+                  errors={errors}
+                  register={register}
+                  rules={{
+                    required: '地址為必填',
+                  }}
+                ></Input>
               </div>
             </div>
             <div className='d-flex flex-column-reverse flex-md-row mt-4 justify-content-between align-items-md-center align-items-end w-100'>
@@ -85,12 +121,16 @@ function Checkout() {
               <h4 className='mb-4'>選購餐點</h4>
               {cartData?.carts?.map((item) => {
                 return (
-                  <div className='d-flex' key="item.id">
+                  <div className='d-flex' key='item.id'>
                     <img
                       src={item.product.imageUrl}
                       alt=''
                       className='me-2'
-                      style={{ width: '48px', height: '48px', objectFit: 'cover' }}
+                      style={{
+                        width: '48px',
+                        height: '48px',
+                        objectFit: 'cover',
+                      }}
                     />
                     <div className='w-100'>
                       <div className='d-flex justify-content-between fw-bold'>
@@ -105,7 +145,7 @@ function Checkout() {
                       </div>
                     </div>
                   </div>
-                )
+                );
               })}
               <div className='d-flex justify-content-between mt-4'>
                 <p className='mb-0 h4 fw-bold'>Total</p>
