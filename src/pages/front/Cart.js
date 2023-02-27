d9939272e33a0d89bfae5c8ac97d2c86eb36257d
wcc723 <agito723@gmail.com>
2023-02-27 10:17:34 +0800

    更新購物車數量

--- src/pages/front/Cart.js
+++ src/pages/front/Cart.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 function Cart() {
   const { cartData, getCart } = useOutletContext();
+  const [loadingItems, setLoadingItem] = useState([])
 
   const removeCartItem = async (id) => {
     try {
@@ -15,6 +17,29 @@ function Cart() {
       console.log(error);
     }
   }
+
+  const updateCartItem = async (item, quantity) => {
+    const data = {
+      data: {
+        product_id: item.product_id,
+        qty: quantity,
+      },
+    };
+    setLoadingItem([...loadingItems, item.id])
+    try {
+      const res = await axios.put(
+        `/v2/api/${process.env.REACT_APP_API_PATH}/cart/${item.id}`,
+        data,
+      );
+      console.log(res);
+      setLoadingItem(
+        loadingItems.filter((loadingObject) => loadingObject !== item.id),
+      );
+      getCart();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className='container'>
@@ -39,11 +64,11 @@ function Cart() {
                 />
                 <div className='w-100 p-3 position-relative'>
                   <button
-                    type="button"
+                    type='button'
                     className='position-absolute btn'
                     style={{ top: '10px', right: '10px' }}
                     onClick={() => {
-                      removeCartItem(item.id)
+                      removeCartItem(item.id);
                     }}
                   >
                     <i className='bi bi-x-lg'></i>
@@ -54,24 +79,24 @@ function Cart() {
                   </p>
                   <div className='d-flex justify-content-between align-items-center w-100'>
                     <div className='input-group w-50 align-items-center'>
-                      <div className='input-group-prepend pe-1'>
-                        <a href='#'>
-                          {' '}
-                          <i className='fas fa-minus'></i>
-                        </a>
-                      </div>
-                      <input
-                        type='text'
-                        className='form-control border-0 text-center my-auto shadow-none bg-light px-0'
-                        placeholder=''
-                        aria-label='Example text with button addon'
-                        aria-describedby='button-addon1'
-                      />
-                      <div className='input-group-append ps-1'>
-                        <a href='#'>
-                          <i className='fas fa-plus'></i>
-                        </a>
-                      </div>
+                      <select
+                        name=''
+                        className='form-select'
+                        id=''
+                        value={item.qty}
+                        disabled={loadingItems.includes(item.id)}
+                        onChange={(e) => {
+                          updateCartItem(item, e.target.value * 1);
+                        }}
+                      >
+                        {[...new Array(20)].map((i, num) => {
+                          return (
+                            <option value={num + 1} key={num}>
+                              {num + 1}
+                            </option>
+                          );
+                        })}
+                      </select>
                     </div>
                     <p className='mb-0 ms-auto'>NT${item.final_total}</p>
                   </div>
